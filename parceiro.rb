4d0b904231b7215f6faa8259261e98f27db862f8
rmparanhos <raffaelmp@id.uff.br>
2018-03-24 15:56:36 -0300

    Teste

--- parceiro.rb
+++ parceiro.rb
@@ -1,5 +1,16 @@
 require 'parslet' #gem indicada
 
+=begin
+P1 - 25 e 27/04 - Expressões e comandos.
+    Objetivos:
+    1-Ambientar-se com a linguagem escolhida e suas ferramentas.
+    2-Entender PEG.
+    3-Entender SMC.
+    4-Implementar BPLC-mark0: (i) O tipo de dados SMC, (ii) operações aritméticas, Booleanas e comandos.
+    5-Implementar um parser PEG para operações aritméticas, Booleanas e comandos.
+    6-Implementar um compilador de AST PEG para operações aritméticas, Booleanas e comandos para BPLC-mark0.
+=end
+
 class Parceiro < Parslet::Parser
   rule(:integer) { match('[0-9]').repeat(1) } #logica do parser para reconhecer apenas numeros
   root(:integer)
