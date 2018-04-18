f4e3c6a17fd235cc712bc482fe9ae85a9d9d1563
Julia <juliafsfalcao@hotmail.com>
2018-04-17 21:57:43 -0300

    makefile e install.sh atualizados

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+.PHONY: install uninstall
+
 install:
 	./install.sh
 
+uninstall:
+	gem uninstall parslet
+	rbenv uninstall 2.5.1
+	brew uninstall rbenv
--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
-# shell script to install the ruby language and the parslet library
+# shell script to install the ruby language and the parslet library on macOS
 
+# install rbenv through homebrew
 brew update
-brew install ruby
+brew install rbenv
 
-gem update --system
-gem install parslet # 1st draft; might not work because of permissions
+# install ruby 2.5.1 through rbenv
+eval "$(rbenv init -)"
+rbenv install 2.5.1
+rbenv rehash
+rbenv global 2.5.1
+
+# install the parslet gem
+gem install parslet
