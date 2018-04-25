4b35d936e2bdc1a7c350b6e276e493a265cc4f47
Wallace Baleroni <wallacebaleroni@gmail.com>
2018-04-24 22:50:24 -0300

    parceiro.rb removido

--- ruby/parceiro.rb
+++
@@ -1,9 +0,0 @@
-require 'parslet' #gem indicada
-
-class Parceiro < Parslet::Parser
-  rule(:integer) { match('[0-9]').repeat(1) } #logica do parser para reconhecer apenas numeros
-  root(:integer)
-end
-
-Parceiro.new.parse("1234") #é aceita pela linguagem
-#Parceiro.new.parse("Hello World!!"), nao é aceita e gera um erro caso seja executada
