f32318d7713354e73104020ba200f8200c35992d
rmparanhos <raffaelmp@id.uff.br>
2018-04-25 23:12:18 -0300

    socorr

--- ruby/bplc.rb
+++ ruby/bplc.rb
@@ -5,8 +5,44 @@ class BPLC
   def vamosRodar(smc)
     puts("Autobots, let's roll!")
     while(smc.tamPilhaControle > 0)
-      val = smc.topoControle()
+      val = smc.desempilhaControle()
       if(val.is_a?(Fixnum))
+        smc.en()
+      elsif(val.is_a?(String))
+        if(val == 'assign')
+          smc.ce()
+        elsif(val == 'print')
+          smc.print()
+        elsif(val == 'mul' or val == 'div' or val == 'add' or val == 'sub')
+          smc.ee()
+        elsif(val == 'eq' or val == 'gt' or val == 'ge' or val == 'lt' or val == 'le')
+          smc.be()
+        elsif(val == 'true' or val == 'false')
+          smc.bt()
+        elsif(val == 'neg')
+          smc.bnote()
+        elsif(val == 'loop')
+          smc.cwhilee()
+        elsif(val == 'if')
+          smc.cife()
+        elsif(val == 'fimloop'or val == 'fimif' or val =='fimelse')
+          smc.cnil()
+        elsif(val == ';')
+          smc.csc()
+        else
+          smc.en()
+        end
+      else
+          puts(val)
+          val.eval()
+      end
+    puts("THE END")
+    end
+  end
+end
+
+=begin
+if(val.is_a?(Fixnum))
         smc.en()
       elsif(val.is_a?(String))
         if(val == 'assign')
@@ -35,7 +71,4 @@ class BPLC
       elsif(val.nil?)
         smc.cnil()
       end
-    end
-    puts("THE END")
-  end
-end
+=end
