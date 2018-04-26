73fb9cd372aa8c47e7b3655c93b42f8136873687
Julia <juliafsfalcao@hotmail.com>
2018-04-26 20:22:40 -0300

    tree

---
+++ ruby/tree.rb
@@ -0,0 +1,43 @@
+require 'parslet'
+
+class Tree
+    @id = nil
+    @children = nil
+
+    def initialize(id, children=[])
+        @id = id
+        @children = children
+    end
+    
+    def leaf?()
+        @children.length == 0
+    end
+
+    def insert(item)
+        if item.is_a?(Tree)
+            @children.add(item)
+
+        else
+            @children = @children.push(Tree.new(item))
+        end
+    end
+
+
+    def to_s()
+        "<id=#{@id}, children=#{@children}>"
+    end
+    
+        
+
+    def children()
+        @children
+    end
+
+    def id()
+        @id
+    end
+
+end
+
+add1 = Tree.new("add", [1, Tree.new("add", [2, 3])])
+puts add1
