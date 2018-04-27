5f1293583fd71e01f8f85d1f3e4d386f53839e62
Wallace Baleroni <wallacebaleroni@gmail.com>
2018-04-27 04:21:12 -0300

    mergeegegegeg

--- ruby/main.rb
+++ ruby/main.rb
@@ -3,17 +3,32 @@ require_relative 'bumblebee'
 require_relative 'bplc'
 require_relative 'smc'
 
-code = "proc fact(x) {
+#=begin
+$smc = SMC.new
+bplc = BPLC.new
+Bumblebee.new.apply(OptimusParser.new.rollOut("
+proc fact(x) {
   x := 6;
   y := 1;
-  while (~ x == 0) do {
+  while (x > 0) do {
       y := y * x;
       x := x - 1
   };
   print(y)
-}"
+}
+  "))
+bplc.vamosRodar($smc)
+#=end
 
+=begin
 $smc = SMC.new
 bplc = BPLC.new
-Bumblebee.new.apply(OptimusParser.new.rollOut(code))
+Bumblebee.new.apply(OptimusParser.new.rollOut("
+proc fact(x) {
+  x := 6;
+  y := 1;
+  print(x * 2 + 1)
+}
+"))
 bplc.vamosRodar($smc)
+=end
